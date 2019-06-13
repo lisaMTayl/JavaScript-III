817b66f787e203554060b466be9474e0459d34b0
Lisa <lmkirk@gmail.com>
2019-06-13 17:59:43 -0400

    I'm totally confused.  Going to rewatch class video

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -15,6 +15,13 @@
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
+function GameObject(attributes) {
+    this.createdAt = attributes.createdAt;
+    this.name = attributes.name;
+    this.dimensions = attributes.dimensions;
+    this.destroy = function () {
+        return `${this.name} was removed from the game`;
+    };
 
 
 /*
